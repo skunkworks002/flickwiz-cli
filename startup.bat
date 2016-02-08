1c06877883c40bc4ffd5638c717d109966317a74
ZubairOrbit <zubairjaved74@gmail.com>
2016-02-08 15:04:07 +0500

    script files

---
+++ startup.bat
@@ -0,0 +1,10 @@
+@echo off
+:title           :startup.sh
+:description     :Batch script to run Flickwiz Project.
+:author		 	 :orbit software solutions
+:date            :2015-11-13
+:version         :1.0    
+:usage		 	 :startup.bat
+:==============================================================================
+
+java  -jar ./target/flickwiz-v1.0-jar-with-dependencies.jar
---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#title           :startup.sh
+#description     :Shell script to run Flickwiz Project.
+#author		 	 :orbit software solutions
+#date            :2015-11-10
+#version         :1.0    
+#usage		 	 :bash startup.sh
+#==============================================================================
+java  -jar ./target/flickwiz-v1.0-jar-with-dependencies.jar
